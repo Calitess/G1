9f21f6807ca79eb848a22d3b2f82a458de0bb9e5
Calitess <nizuainas@gmail.com>
2023-08-02 18:33:46 +1000

    Got Journal to be in a camera stack

---
+++ Experimentations/Assets/Experimentation/Scripts/CameraStacking.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CameraStacking : MonoBehaviour
+{
+    [SerializeField] Camera camera, myOverlayCamera;
+    [SerializeField] bool journalOpened;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q) && journalOpened)
+        {
+            var cameraData = camera.GetUniversalAdditionalCameraData();
+            cameraData.cameraStack.Add(myOverlayCamera);
+            journalOpened = false;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Q) && !journalOpened)
+        {
+            var cameraData = camera.GetUniversalAdditionalCameraData();
+            cameraData.cameraStack.Remove(myOverlayCamera);
+            journalOpened = true;
+
+        }
+    }
+}
