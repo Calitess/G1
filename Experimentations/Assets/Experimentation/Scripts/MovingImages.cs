dc9ba1b85f453dfcaa465a1eb705bb6d9e851d47
Calitess <110515459+Calitess@users.noreply.github.com>
2023-08-07 12:56:20 +1000

    Parallax menu implemented

---
+++ Experimentations/Assets/Experimentation/Scripts/MovingImages.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingImages : MonoBehaviour
+{
+    private Transform menuImagesToMove;
+    [SerializeField] float randomValue, durationForLerp;
+    private Vector3 pointsToMove, curPoint, oriPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuImagesToMove = this.GetComponent<Transform>();
+        curPoint = menuImagesToMove.localPosition;
+        oriPoint = menuImagesToMove.localPosition;
+        StartCoroutine(LerpImages(randomValue, durationForLerp));
+        //Debug.Log("image:" + menuImagesToMove + "cur:" + curPoint + "togo:" + pointsToMove);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    IEnumerator LerpImages(float randomVal, float duration)
+    {
+        pointsToMove = new Vector3(Random.Range(oriPoint.x - randomVal, oriPoint.x+randomVal), Random.Range(oriPoint.y - randomVal, oriPoint.y+randomVal),oriPoint.z);
+        float time = 0;
+        curPoint = menuImagesToMove.localPosition;
+        while (time < duration)
+        {
+            menuImagesToMove.localPosition = Vector3.Lerp(curPoint, pointsToMove, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+
+            //Debug.Log("image:" + menuImagesToMove + "cur:" + curPoint + "togo:" + pointsToMove);
+        }
+        menuImagesToMove.localPosition = pointsToMove;
+
+
+        yield return new WaitForSecondsRealtime(0);
+
+        StartCoroutine(LerpImages(randomValue, durationForLerp));
+    }
+}
